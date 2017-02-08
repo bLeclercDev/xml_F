a04117b4b32cb752d985221cec5fd7e97ac75a54
Billy Villena <Edhrendal@users.noreply.github.com>
2017-02-08 14:19:42 +0100

    CSS a XML File
    See the differences between a simple XML file and a XML file with a stylesheet.

---
+++ XML/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>XML - Transformation de documents XML</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+    $home = true;
+    include_once 'templates/navbar.php';?>
+</body>
+</html>
---
+++ XML/templates/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fichiers XML <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="xml/jeux-sans-css.xml">XML sans css</a></li>
+                        <li><a href="xml/jeux-avec-css.xml">XML avec css</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="<?php echo isset($xmlToXml) ? 'active' : '';?>"><a href="xmlToXml.php">Transformation XSL d'un XML en XML</a></li>
+                        <li><a href="jeux-avec-css.xml">XML avec css</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="<?php echo isset($home) ? 'active' : '';?>"><a href="index.php">Accueil</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
