e4f8d557c9e5c9854248a3da924fbaeef749c9e1
Billy Villena <Edhrendal@users.noreply.github.com>
2017-02-13 11:29:42 +0100

    Clean 2

--- XML/xmlToPDF.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $content = file_get_contents('t3.html');
-
-    require_once('html2pdf/vendor/autoload.php');
-    $html2pdf = new HTML2PDF('P','A4','fr');
-    $html2pdf->WriteHTML($content);
-    $html2pdf->Output('exemple.pdf');
-?>
